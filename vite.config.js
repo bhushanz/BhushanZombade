cdb6a34762931667676a483d8b7aa0fc02a37ba9
user.name mrbhushan006@gmail.com <mrbhushan006@gamil.com>
2025-10-02 09:18:59 +0530

    new chnage

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-     tailwindcss(),
-    react()],
-})
+
+
+
+export default defineConfig(() => ({
+  plugins: [   tailwindcss(),react()],
+  base:"/",
+}));
